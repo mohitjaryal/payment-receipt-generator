9f5e6001e0413a31b1de349e2506ecaee0cea878
Mohit <mohitt.tech@gmail.com>
2026-03-18 18:34:59 +0530

    recipt generator program done

--- src/recipts.py
+++ src/recipts.py
@@ -3,3 +3,45 @@ from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, TableStyle
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import getSampleStyleSheet
+from datetime import datetime
+import os
+
+# generate recipt function
+def generate_recipt(name, product, duration, price):
+
+    # date
+    date = datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+
+    # generated output will store here
+    filename = f"output/recipt_{date}.pdf"
+
+    os.makedirs("output/", exist_ok=True)
+
+    DATA=[
+        ["Date", "Custumer", "Product", "Duration", "Price(Rs)"],
+        [date, name, product, duration, price]
+    ]
+
+    # converting generated recipt into pdf
+    pdf = SimpleDocTemplate(filename, pagesize = A4)
+
+    # style
+    styles = getSampleStyleSheet()
+    title_style = styles['Heading 1']
+    title_style.alignment = 1
+
+    title = Paragraph('Payement Recipt', title_style)
+
+    style = TableStyle([
+        ('BOX',(0,0), (-1,-1), 1 colors.black),
+        ('GRID',(0,0), (-1,-1), 1 colors.black),
+        ('BACKGROUND',(0,0), (-1, 0) colors.black),
+        ('ALIGN',(0,0), (-1,-1) "CENTER"),
+    ])
+
+    table = Table(DATA)
+    table.setStyle(style)
+
+    pdf.build([title, table])
+
+    return filename
